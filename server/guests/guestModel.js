80bfe6e82fb2f57d09b466a767f63d9c0e13bb67
Michiya Hibino <mphibino@gmail.com>
2017-03-24 21:36:38 -0400

    feat(server routes for guests): add server routes for guests
    routes for getGuests, addGuest, removeGuest

--- server/guests/guestModel.js
+++ server/guests/guestModel.js
@@ -7,8 +7,37 @@ module.exports = {
 		.innerJoin('users', 'user_events.user_id', 'users.id')
 		.where('event_id', eventId)
 		.then(function(guests) {
-			console.log('gm-L10-guests: ', guests);
+			// console.log('gm-L10-guests: ', guests);
 			return guests;
+		})
+		.catch(function(err) {
+			console.error(err);
+		});
+	},
+
+	addGuest: function(eventId, guestId) {
+			
+		return db.insert({user_id: guestId, event_id: eventId})
+		.into('user_events')
+		.then(function(newGuest) {
+			// console.log('-----GM-L31-Guest Added: ', newGuest);
+			return newGuest[0];
+			// res.send({error: false, data: newGuest});
+		})
+		.catch(function(err) {
+			console.error(err);
+		});
+	},
+
+	removeGuest: function(guestId, eventId) {
+		// console.log('GM-L33-Guest-ID: ', guestId, 'Event-ID: ', eventId);
+		return db('user_events').where({
+			'user_id': guestId,
+			'event_id': eventId
+		}).del()
+		.then(function(count) {
+			// console.log('GM-L39-count: ', count);
+			return count;
 		})
 		.catch(function(err) {
 			console.error(err);
@@ -16,45 +45,3 @@ module.exports = {
 	}
 };
 
-// module.exports = {
-
-// 	getGuests: function(eventId) {
-// 		console.log('GM-L6-getGuests aka gG: ', 'eventId ', eventId);
-// 		return db.select('users.name', 'users.email', 'users.profile_url').from('user_events')
-// 		// .innerJoin('users', 'user_events.user_id', 'users.id')
-// 		.innerJoin('events', 'user_events.event_id', 'events.id')
-// 		.where('event_id', eventId)
-// 			// 'host_id': (db.select('id').from('users').where('email', userEmail)) 
-// 		// })
-// 		.then(function(guests) {
-// 			console.log('These are the guests', guests);
-// 			return guests;
-// 			// cb(null, guests);
-// 		})
-// 		.catch(function(err) {
-// 			console.error(err);
-// 		});
-// 	}
-
-	// addGuest: function(guestEmail, eventId, cb) {
-	// 	db('user_events').insert({
-	// 		user_id: (db.select('id').from('users').where('email', guestEmail)),
-	// 		event_id: eventId
-	// 	})
-	// 	.then(function(newGuest) {
-	// 		console.log('GM-L28: newGuest: ', newGuest);
-	// 		cb(null, newGuest);
-	// 	})
-	// 	.catch(function(err) {
-	// 		cb(err);
-	// 	});
-	// }
-// };
-
-// getAttendees: function(eventID, callback) {
-// 		db.select().from('user_events').innerJoin('users', 'user_events.user_id', 'users.id').where('event_id', eventID)
-// 		.then(function(userID){
-// 			console.log("This is what is returned from inner join table call ", userID)
-// 			callback(userID);
-// 		})
-// 	},
